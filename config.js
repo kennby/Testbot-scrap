8d96e1e1b6daccbb1af0cfbcc1451b6e0fd79e40
kennby <153956421+kennby@users.noreply.github.com>
2024-05-19 22:16:32 -0500

    Update config.js

--- config.js
+++ config.js
@@ -10,15 +10,15 @@ import moment from 'moment-timezone'
 //*꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒦꒷*
 
 global.owner = [
-  ['5218261275256', 'おDanịel.xyz⁩', true],
-  ['5492616532494', 'Samuel Fts', true],
-  ['51910108980'],
-  ['5218139760662'],
-  ['5218132588591'],
-  ['5218261275256'],
-  ['5218261009198'],
-  ['50662333781'],
-  ['5218261000681']
+  ['51907364682', 'kenn', true],
+  ['', '', false],
+  [''],
+  [''],
+  [''],
+  [''],
+  [''],
+  [''],
+  ['']
 
 ]
 
@@ -30,18 +30,18 @@ global.prems = []
 //*꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒦꒷*
 
 global.packname = `Sumi Sakurasawa - MD`
-global.author = 'おDanịel.xyz'
+global.author = 'KENN'
 global.botname = 'Sumi Sakurasawa - MD'
-global.textbot = `© 2024 Starlights Team | All rights reserved`
+global.textbot = ``
 
 //*꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒦꒷*
 
-global.group = 'https://chat.whatsapp.com/DaZrhN5469XDs6lBNmcdOO'
-global.group2 = 'https://chat.whatsapp.com/DavHccRC8t64mUUQXK4OrU'
-global.group3 = 'https://chat.whatsapp.com/GFqGm6fI8ANDaIVZ2mQciQ'
-global.group4 = 'https://chat.whatsapp.com/CqdWTXmS702JD31SQzr0Ph'
-global.group5 = 'https://chat.whatsapp.com/G0JPb1IvBwUDVFqm5YUiGA'
-global.canal = 'https://whatsapp.com/channel/0029VaBfsIwGk1FyaqFcK91S'
+global.group = 'https://whatsapp.com/channel/0029VaOXYA76GcGCoumjn41j'
+global.group2 = 'https://whatsapp.com/channel/0029VaOXYA76GcGCoumjn41j'
+global.group3 = 'https://whatsapp.com/channel/0029VaOXYA76GcGCoumjn41j'
+global.group4 = 'https://whatsapp.com/channel/0029VaOXYA76GcGCoumjn41j'
+global.group5 = 'https://whatsapp.com/channel/0029VaOXYA76GcGCoumjn41j'
+global.canal = 'https://whatsapp.com/channel/0029VaOXYA76GcGCoumjn41j'
 
 //*꒷꒦꒷꒷꒦꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒷꒦꒷꒷꒦꒷꒷꒦꒷꒦꒷꒦꒷꒦꒷*
 
