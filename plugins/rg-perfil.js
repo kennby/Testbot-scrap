e4e06da21b4c3dc98d00eae5558eca083cd6fc6b
kennby <153956421+kennby@users.noreply.github.com>
2024-05-19 22:18:05 -0500

    Update rg-perfil.js

--- plugins/rg-perfil.js
+++ plugins/rg-perfil.js
@@ -4,7 +4,7 @@ import PhoneNumber from 'awesome-phonenumber'
 let handler = async (m, { conn, usedPrefix, command}) => {
   let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
   let user = global.db.data.users[who]
-  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => './src/avatar_contact.png')
+  let pp = await conn.profilePictureUrl(who, 'image').catch(_ => 'https://qu.ax/hWoW.mp4')
   let { exp, limit, name, registered, regTime, age, level } = global.db.data.users[who]
   let { min, xp, max } = xpRange(user.level, global.multiplier)
   let username = conn.getName(who)
